e71510deb5995a40c7b6e02eba16c9fa29fcb06d
MonicaRafaat13 <162500577+MonicaRafaat13@users.noreply.github.com>
2026-05-07 14:11:23 +0300

    Add files via upload

---
+++ database/migrations/2026_05_06_143943_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->float('rating');
+            $table->text('notes')->nullable();
+            $table->enum('status',['Watching', 'Watched', 'Plan to Watch']);
+            $table->string('poster_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
+
